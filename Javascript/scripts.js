c2101988c1dda5b2e3c5ff1b4937ad97a8a0e435
Derrick Ma <derrickma2005@gmail.com>
2024-04-01 13:09:43 -0400

    Updated Color Modes to persist along with page changes

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -1,17 +1,17 @@
-var currentMode = "dark";
+localStorage.setItem("currentMode", "dark")
 document.getElementById("body").setAttribute("data-bs-theme", currentMode);
 document.getElementById("lightswitchborder").onclick = function() {
-    if (currentMode == "light") {
-        currentMode = "dark";
-        document.getElementById("body").setAttribute("data-bs-theme", currentMode);
+    if (localStorage.getItem("currentMode") == "light") {
+        localStorage.setItem("currentMode", "dark");
+        document.getElementById("body").setAttribute("data-bs-theme", localStorage.getItem("currentMode"));
         document.getElementById("LinkedinLogo").setAttribute("src", "Images/Linkedin.png");
         document.getElementById("GithubLogo").setAttribute("src", "Images/GitHub.png");
         document.getElementById("EmailLogo").setAttribute("src", "Images/Mail_Icon.png");
         setTimeout(function() {document.getElementById("switchicon").setAttribute("class", "lighticon")}, 100);
     }
     else {
-        currentMode = "light";
-        document.getElementById("body").setAttribute("data-bs-theme", currentMode);
+        localStorage.setItem("currentMode", "light");
+        document.getElementById("body").setAttribute("data-bs-theme", localStorage.getItem("currentMode"));
         document.getElementById("LinkedinLogo").setAttribute("src", "Images/LinkedinLight.png");
         document.getElementById("GithubLogo").setAttribute("src", "Images/GitHubLight.png");
         document.getElementById("EmailLogo").setAttribute("src", "Images/Mail_Icon_Light.png");
